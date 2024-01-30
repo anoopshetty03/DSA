df99eb9da982e184eb14a265b1c9003b2fce19a2
anoopshetty03 <148759181+anoopshetty03@users.noreply.github.com>
2024-01-30 22:29:25 +0530

    Update diameter.cpp

--- Trees/Binary Trees/diameter.cpp
+++ Trees/Binary Trees/diameter.cpp
@@ -13,19 +13,6 @@ struct Node
 }; */
 
 class Solution {
-  private:
-    int height(struct Node* node){
-        //base case
-        if(node == NULL) {
-            return 0;
-        }
-        
-        int left = height(node ->left);
-        int right = height(node->right);
-        
-        int ans = max(left, right) + 1;
-        return ans;
-    }
   public:
     // Function to return the diameter of a Binary Tree.
     

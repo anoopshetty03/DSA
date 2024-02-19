8eafbf7363f652b1ebbda33d94d870dd007dbd2b
anoopshetty03 <148759181+anoopshetty03@users.noreply.github.com>
2024-02-19 12:26:40 +0530

    Update powerOfTwo.cpp

--- Bit Manipulation/powerOfTwo.cpp
+++ Bit Manipulation/powerOfTwo.cpp
@@ -3,20 +3,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-        int ans = 1;
-        
-        for(int i = 0; i <= 30; i++) {
-            
-            //cout<<" ans "<<ans <<endl;
-            if(ans == n)
-            {
-                return true;
-            }
-            if(ans < INT_MAX/2)
-            ans = ans * 2;   
-            
-        }
+       // T.C -> O(logn)
+       if(n == 0)
         return false;
+
+       while(n%2 == 0)  
+           n /=2;       
+
+        return n == 1;  
     }
 };
